bf54179fac078bbbd3e8d79556dc3673b9562761
Calvin Roberts <you@example.com>
2023-06-27 16:03:58 -0600

    installed express and updated server.js

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-const express = require('express')
-const session = require('express-session')
-const handlebars = require('express-handlebars')
-const routes = require();
-
-const sequelize = require('./config/connection')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const express = require("express");
+const session = require("express-session");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const expHandlebars = require("express-handlebars");
+const handlebars = expHandlebars.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
+
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
 
 app.use(session(sess));
 
@@ -27,5 +30,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
